54e6bcbe6d79cc76318353a82636c2df5e96d001
Tyler Bolty <tsbolty@gmail.com>
2021-03-19 15:14:02 -0600

    example of on change

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
 import React, { useState } from 'react';
 import './App.css';
-import gtfs from 'gtfs-stream';
-import request from 'request';
+import MyComponent from './components/MyComponent';
 
 function App() {
-  const [state, setState] = useState()
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-  const url = 'https://www.rtd-denver.com/files/gtfs/google_transit.zip';
+  const [stateName, setStateName] = useState("")
+  const [hungry, setHungry] = useState(false)
 
-  request.get(proxyUrl + url)
-    .pipe(gtfs.enhanced())
-    .on('data', (entity) => {
-      if (entity.data.trip_id == 113631947) {
-        // console.log(entity)
-        setState(entity.data.path.coordinates[entity.data.path.coordinates.length - 1])
-        // entity.data.path.coordinates.map(item =>{
-        //   console.log(`latitude: ${item[1]} 
-        //   longitude: ${item[0]}`)
-        // })
-      }
-    })
+  const handleInputChange = event => {
+    // const {name, value} = event.target;
+    // setStateName({ ...stateName, [name]: value})
+    setStateName(event.target.value)
+  }
+
+  const handleCheck = e => {
+    setHungry(!hungry)
+  }
 
   return (
     <div className="App">
-      {state &&
-        <>
-          <h5>Latitude: {state[1]}</h5>
-          <h5>Longitude: {state[0]}</h5>
-        </>}
+      <h2>hello</h2>
+      <input type="checkbox" onClick={handleCheck} />
+      <input placeholder="type here" name="key" onChange={handleInputChange}/>
+      <MyComponent fullName={stateName} hungry={true} />
     </div>
   );
 }
---
+++ src/components/MyComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyComponent = (props) => {
+  return(
+    <h3>hello:</h3>
+  )
+}
+
+export default MyComponent;
