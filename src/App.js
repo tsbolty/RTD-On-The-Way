cbd9ab2709b7db640ccc242385ec859d0f5cdd76
Tyler Bolty <tsbolty@gmail.com>
2021-06-28 00:05:06 -0600

    can mostly search along route

--- src/App.js
+++ src/App.js
@@ -11,8 +11,11 @@ function App() {
 	});
 	const [stops, setStops] = useState([]);
 	const [origin, setOrigin] = useState("");
-	const [destination, setDestination] = useState();
+	const [destination, setDestination] = useState("");
 	const [lineChosen, setLineChosen] = useState("");
+	const [keywordSearch, setKeywordSearch] = useState("");
+	const [distanceSearch, setDistanceSearch] = useState("");
+	const [markers, setMarkers] = useState([]);
 
 	useEffect(() => {
 		navigator.geolocation.getCurrentPosition((location) =>
@@ -24,6 +27,39 @@ function App() {
 		);
 	}, []);
 
+	const handleSearch = async () => {
+		setStops(routing.getStops(lineChosen, origin, destination));
+
+		let results = [];
+		const allData = await stops.map(async (stop) => {
+			return routing
+				.searchPlaces(
+					keywordSearch,
+					distanceSearch,
+					stop.coordinates[1],
+					stop.coordinates[0]
+				)
+				.then((res) => res.data.results)
+				.catch((err) => console.log(err));
+		});
+		Promise.all(allData).then((values) => {
+			values.forEach((value) => {
+				const objects = value.map((obj) =>
+					results.push({
+						name: obj.name,
+						lat: obj.geometry.location.lat,
+						lon: obj.geometry.location.lng
+					})
+				);
+				const filteredObjects = objects.filter(
+					(item) => item.name && item.lat && item.lon
+				);
+				results.push(...filteredObjects);
+			});
+			setMarkers(results);
+		});
+	};
+
 	return (
 		<div>
 			<div style={{ display: "flex", justifyContent: "space-around" }}>
@@ -32,7 +68,9 @@ function App() {
 						setLineChosen(e.target.value.slice(0, 1).toLowerCase())
 					}>
 					{allLines.map((line) => (
-						<option value={line}>{line}</option>
+						<option key={Math.floor(Math.random() * 100000)} value={line}>
+							{line}
+						</option>
 					))}
 				</select>
 				{lineChosen.length ? (
@@ -41,30 +79,78 @@ function App() {
 							<h2>Origin</h2>
 							<select onChange={(e) => setOrigin(e.target.value)}>
 								{routing.getLine(lineChosen).map((station) => (
-									<option value={station.name}>{station.name}</option>
+									<option
+										key={Math.floor(Math.random() * 100000)}
+										value={station.name}>
+										{station.name}
+									</option>
 								))}
 							</select>
 						</div>
 						<div style={{ display: "flex" }}>
 							<h2>Destination</h2>
-							<select onChange={(e) => setDestination(e.target.value)}>
+							<select
+								onChange={(e) => setDestination(e.target.value)}
+								value={destination}>
 								{require(`./utils/${lineChosen}Line.json`).map((station) => (
-									<option value={station.name}>{station.name}</option>
+									<option
+										key={Math.floor(Math.random() * 100000)}
+										value={station.name}>
+										{station.name}
+									</option>
 								))}
 							</select>
 						</div>
-						<button
-							onClick={(e) =>
-								setStops(routing.getStops(lineChosen, origin, destination))
-							}>
-							search
-						</button>
+						<button onClick={(e) => handleSearch()}>search</button>
 					</>
 				) : null}
 			</div>
+			{origin && destination ? (
+				<div style={{ display: "flex" }}>
+					<h2>Search For</h2>
+					<select
+						onChange={(e) => setKeywordSearch(e.target.value)}
+						value={keywordSearch}>
+						<option value='gas_station'>Gas Station</option>
+						<option value='atm'>ATM</option>
+						<option value='supermarket'>Grocery Store</option>
+						<option value='convenience_store'>Convenience Store</option>
+						<option value='drugstore'>Drugstore</option>
+						<option value='restaurant'>Restaurant</option>
+						<option value='meal_takeaway'>Takeout</option>
+						<option value='bar'>Bar</option>
+						<option value='cafe'>Cafe</option>
+						<option value='liquor_store'>Liquor Store</option>
+						<option value='park'>Park</option>
+					</select>
+					<h2>Within</h2>
+					<select
+						onChange={(e) =>
+							setDistanceSearch(Math.floor(parseInt(e.target.value) * 80.4672))
+						}
+						value={distanceSearch}>
+						<option value={1}>1</option>
+						<option value={2}>2</option>
+						<option value={3}>3</option>
+						<option value={4}>4</option>
+						<option value={5}>5</option>
+						<option value={6}>6</option>
+						<option value={7}>7</option>
+						<option value={8}>8</option>
+						<option value={9}>9</option>
+						<option value={10}>10</option>
+						<option value={11}>11</option>
+						<option value={12}>12</option>
+						<option value={13}>13</option>
+						<option value={14}>14</option>
+						<option value={15}>15</option>
+					</select>
+					<p>blocks</p>
+				</div>
+			) : null}
 			<div>
 				{center.userLocation ? (
-					<GoogleMap center={center} zoom={12} markers={stops} />
+					<GoogleMap center={center} zoom={12} markers={markers} />
 				) : null}
 			</div>
 		</div>
--- src/utils/route.js
+++ src/utils/route.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 const getStops = (line, origin, destination) => {
 	const allLineStops = require(`./${line.toLowerCase()}Line.json`);
 	const start = allLineStops.map((e) => e.name).indexOf(origin);
@@ -10,6 +11,11 @@ const getLine = (line) => {
 	return require(`./${line}Line.json`);
 };
 
-const methods = { getStops, getLine };
+const searchPlaces = (name, distance, lat, lon) =>
+	axios.get(
+		`/api/maps/place/${name}/${distance}/${lat}/${lon}/${process.env.REACT_APP_GOOGLE_API_KEY}`
+	);
+
+const methods = { getStops, getLine, searchPlaces };
 
 export default methods;
